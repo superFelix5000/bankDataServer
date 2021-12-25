c553c927cbadbe21ecfca627df1a1ae13f53ba37
pixelschubs <fwest@posteo.de>
2021-12-25 20:43:01 +0200

    fixed cors for angular app

--- server.ts
+++ server.ts
@@ -1,10 +1,14 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 import router from "./routes.ts";
 
 const port = 8000;
 
 const app = new Application();
 
+app.use(oakCors({
+  origin: "http://localhost:4200",
+}));
 app.use(router.routes());
 app.use(router.allowedMethods());
 
